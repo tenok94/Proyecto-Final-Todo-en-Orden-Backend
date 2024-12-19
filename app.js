c61d5f0a45a39285623554b8b37865a70b92e51b
tenok94 <tenok94@gmail.com>
2024-12-19 03:28:23 -0300

    l

--- app.js
+++ app.js
@@ -1,46 +1,3 @@
-// require("dotenv").config();
-// const express = require("express");
-// const app = express();
-// const dbconnect = require("./config/db.js");
-// const TareasRouter = require("./routes/tareas.js");
-// const cors = require("cors");
-
-// app.use(cors());
-// app.use(express.json());
-// app.use(TareasRouter);
-
-// app.get("/", (req, res) => {
-//     res.send("Servidor de API de tareas en servicio");
-// })
-
-// dbconnect().then(() => {
-//     app.listen(3000, () => {
-//         console.log("Server corriendo en el puerto 3000");
-//     });
-// }).catch(err  => {
-//     console.error("no se pudo iniciar el servido debido a un error en la base de datos")
-// });
-// require("dotenv").config(); // Importar dotenv para usar variables de entorno
-// const express = require("express");
-// const app = express();
-// const dbconnect = require("./config/db.js");
-// const TareasRouter = require("./routes/tareas.js");
-// const cors = require("cors");
-
-// app.use(cors());
-// app.use(express.json());
-// app.use(TareasRouter);
-
-// // Iniciar servidor en el puerto configurado
-// dbconnect().then(() => {
-//     const PORT = process.env.PORT || 3000; // Usa el puerto desde el .env o el 3000 por defecto
-//     app.listen(PORT, () => {
-//         console.log(`Server corriendo en el puerto ${PORT}`);
-//     });
-// }).catch(err  => {
-//     console.error("No se pudo iniciar el servidor debido a un error en la base de datos");
-// });
-
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
@@ -50,7 +7,7 @@ const app = express();
 
 // Configuración de CORS
 const corsOptions = {
-    origin: "https://proyecto-final-todo-en-orden-frontend.vercel.app", // Dominio del frontend en Vercel
+    origin: "https://proyecto-final-todo-en-orden-frontend-vkrzenqro.vercel.app/", // Dominio del frontend en Vercel
     methods: "GET,POST,PUT,DELETE", // Métodos permitidos
     allowedHeaders: "Content-Type,Authorization", // Cabeceras permitidas
 };
